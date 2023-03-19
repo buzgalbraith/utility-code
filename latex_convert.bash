55516d8c42db450b1da75c0ae866c4a38250cec4
buz galbraith <buzgalbraith@gmail.com>
2023-03-19 19:06:57 -0400

    added the start of the python

--- latex_convert.bash
+++ latex_convert.bash
@@ -6,5 +6,10 @@ echo "local or overleaf" $4;
 cd $3;
 cp $3$2 $3$2-$4;
 echo $3;
+python /home/buzgalbraith/school/utility-code/latex_convert.py $1 $2 $3 $4
 ls;
 # bash latex_convert.bash /home/buzgalbraith/school/spring_2023/probaility-theroy-2  notes.tex /home/buzgalbraith/school/spring_2023/probaility-theroy-2/notes/week_1/ overleaf
+
+
+## so far we have coppied the file. 
+## now we need a python script 
---
+++ latex_convert.py
@@ -0,0 +1,4 @@
+import sys
+import os 
+repo_path, file_name, file_directory, local= sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4] 
+    

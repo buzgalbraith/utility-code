bd567d6ce3af216fdb7511bb86807c980c9a7f93
buz_desktop_home_WSL <buzgalbraith@gmail.com>
2023-03-19 23:59:43 -0400

    got latex_conver running

--- latex_convert/latex_convert.bash
+++ latex_convert/latex_convert.bash
@@ -3,13 +3,15 @@ echo "repo path: $1";
 echo "file name" $2 
 echo "file directory: $3";
 echo "local or overleaf" $4;
+current_directory="$(pwd)"
 cd $3;
-cp $3$2 $3$2-$4;
+cp $3$2.tex $3$2-$4.tex;
 echo $3;
-python /home/buzgalbraith/school/utility-code/latex_convert.py $1 $2 $3 $4
-ls;
-# bash latex_convert.bash /home/buzgalbraith/school/spring_2023/probaility-theroy-2  notes.tex /home/buzgalbraith/school/spring_2023/probaility-theroy-2/notes/week_1/ overleaf
+python $current_directory/latex_convert.py $1 $2 $3 $4;
+echo "check this" $3$2-$4.tex
 
+pdflatex $3$2$4.tex
+# bash latex_convert.bash /home/buzgalbraith/work/school/spring_2023/probaility-theroy-2  notes /home/buzgalbraith/work/school/spring_2023/probaility-theroy-2/notes/week_3/Video_1:RANDOM-SAMPLING-AND-ESTIMATION-BIAS\ local
 
 ## so far we have coppied the file. 
 ## now we need a python script 
--- latex_convert/latex_convert.py
+++ latex_convert/latex_convert.py
@@ -1,12 +1,30 @@
 import sys
 import re 
-repo_path, file_name, file_directory, local= sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4] 
-texdoc = []  # a list of string representing the latex document in python
+import os 
+import fileinput
+## excpetion handeling. 
+try:
+    repo_path, file_name, file_directory, local= sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4] 
+except:
+    raise Exception("must inlude repo_path, file_name, file_directory and local arguments") 
+if(local!="local" and local!="overleaf"):
+    raise Exception("must specify if is local or on overleaf") 
+
 
 # read the .tex file, and modify the lines
-with open(file_directory+file_name) as fin:
-    for line in fin:
+with fileinput.input(file_directory+file_name+"-"+local+".tex", inplace=True) as f :
+    for line in f:
         if('\\includegraphics' in line):
-            pattern=r'{.*}'
-            match=re.search(pattern, line)
-            print(match.group()[1:-1])
+                if(local=="local"):
+                    pattern=r'{.*}'
+                    match=re.search(pattern, line)
+                    old_string=match.group()[1:-1]
+                    print(re.sub(pattern=pattern,string=line, repl="{"+repo_path+"/"+match.group()[1:-1]+"}"), end="")
+                else:
+                    pattern=r'{.*}'
+                    match=re.search(pattern, line)
+                    old_string=match.group()[1:-1] 
+                    old_string=old_string.replace(repo_path ,"")
+                    edit_line=print(re.sub(pattern=pattern,string=line, repl="{"+old_string[1:]+"}"), end="")
+        else:
+            print(line, end="")
